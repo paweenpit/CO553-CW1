207377519377b0c806959a072740b2e615f9d095
paweenp <paween.pit@gmail.com>
2019-10-21 15:19:29 +0100

    add and remove files

--- decision_tree.py
+++ decision_tree.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def import_clean_data():
-	return np.loadtxt('clean_dataset.txt')
+	return np.loadtxt('./dataset/clean_dataset.txt')
 
 def import_noisy_data():
-	return np.loadtxt('noisy_dataset.txt')
+	return np.loadtxt('./dataset/noisy_dataset.txt')
 
 def H(labels):
 	''' return H-function of labels'''
--- test.py
+++ test.py
@@ -1,19 +1,22 @@
 from decision_tree import *
 
-def test():
-	dat = np.array([
-	    [1,1,1,1,1,1],
-	    [1,1,1,2,2,2],
-	    [3,3,2,2,2,2],
-	    [3,2,1,1,1,1],
-	    [4,4,1,3,4,4],
-	    [1,1,2,3,4,4],
-	    [3,2,3,5,5,7],
-	    [5,6,3,2,7,7]
-	])
+dat = np.array([
+    [1,1,1,1,1,1],
+    [1,1,1,2,2,2],
+    [3,3,2,2,2,2],
+    [3,2,1,1,1,1],
+    [4,4,1,3,4,4],
+    [1,1,2,3,4,4],
+    [3,2,3,5,5,7],
+    [5,6,3,2,7,7]
+])
 
-	print(decision_tree_learning(dat, 0))
+def test(data):
+	tree, depth = decision_tree_learning(data, 0)
+	print('depth: {}'.format(depth))
+	print('tree: {}'.format(tree))
 
-if __name__ == __main__:
-	test()
+if __name__ == '__main__':
+	clean_data = import_clean_data()
+	test(clean_data)
 
