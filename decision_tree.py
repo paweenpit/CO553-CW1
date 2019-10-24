58c6fe3f432b33ff886b524e2822b2a8215a81c0
Niusha Alavi <niushalavi@gmail.com>
2019-10-24 23:18:50 +0100

    visualization

--- decision_tree.py
+++ decision_tree.py
@@ -80,35 +80,38 @@ def decision_tree_learning(dataset, depth=0):
 
 		return root, max(left_depth, right_depth)
 
-def nodes(decision_tree, depth , height , width , x , y ):
+def nodes(decision_tree, depth , x , y , height , width):
 
 	if decision_tree['attribute'] == 0 and decision_tree['value']==0 and decision_tree['left'] == None and decision_tree['right'] == None :
-			plt.text(x, y, "X"+str(decision_tree['attribute'])+"<"+str(decision_tree['value']) , size=height,ha="center", va="center", bbox=dict(boxstyle='round', facecolor='wheat') , fontsize = 8)
+			plt.text(x, y, "X"+str(decision_tree['attribute'])+"<"+str(decision_tree['value']) , size=height,ha="center", va="center", bbox=dict(boxstyle='round', facecolor='white' ) , fontsize =  10 - np.minimum(depth , 5))
 
-	plt.text(x, y, "X"+str(decision_tree['attribute'])+"<"+str(decision_tree['value']) , size=height,ha="center", va="center", bbox=dict(boxstyle='round', facecolor='wheat') , fontsize = 8)
-
+	plt.text(x, y, "X"+str(decision_tree['attribute'])+"<"+str(decision_tree['value']) , size=height,ha="center", va="center", bbox=dict(boxstyle='round', facecolor='white') , fontsize = 10 -  np.minimum(depth , 5))
 	if decision_tree['left'] != None :
-		x_left = x - width/2
+		x_left = x - (1/ (2**(depth+2)))
 		y_left = y - height
-		nodes (decision_tree['left'] , depth+1 , height , width , x_left , y_left )
+		plt.plot([x , x_left] , [ y , y_left] , marker = 'o')
+		nodes (decision_tree['left'] , depth+1  , x_left , y_left , height , width )
 
 	if decision_tree['right'] != None :
-		x_right = x + width/2
+		x_right = x + (1/(2**(depth+2)))
 		y_right = y - height
-		nodes (decision_tree['right'] , depth+1 , height , width , x_right , y_right )
+		plt.plot([x , x_right] , [ y , y_right], marker = 'o')
+		nodes (decision_tree['right'] , depth+1 , x_right , y_right, height , width  )
 
+def visualization () :
 
-decision_tree , _ = decision_tree_learning(import_clean_data(),0)
-depth = 5
-imageSize = 100
-img = np.zeros([imageSize,imageSize,3],dtype=np.uint8)
-img.fill(205)
+	decision_tree , _ = decision_tree_learning(import_clean_data(),0)
+	depth = 5
 
-plt.figure(figsize=(10,10))
-# fig,ax = plt.subplots(1)
-# ax.imshow(img)#
-height = imageSize/depth
-width =  height
-nodes(decision_tree, depth , 0.2 , 0.2, 0.5 , 1 )
-plt.axis('off')
-plt.show()				
+	fig = plt.figure(figsize=(12,6))
+	axes = plt.gca()
+	axes.set_xlim([0,1])
+	axes.set_ylim([0,1])
+	# plt.subplots_adjust(left=0.2, right=1.2, top=1.2, bottom=0.2)
+	height = 1/depth
+	width =  height
+	nodes(decision_tree, 0 , 0.5 , 1 , height , width)
+	plt.axis('off')
+	plt.show()				
+
+visualization()
