bfe8e460a1c1eaea556b6491bc97457a47755c6a
Niusha Alavi <niushalavi@gmail.com>
2019-10-25 00:53:29 +0100

    visualize  Please enter the commit message for your changes. Lines starting

--- visualize.py
+++ visualize.py
@@ -1,23 +1,20 @@
-import matplotlib.lines as mlines
-
 from decision_tree import *
 from util import *
 
 def nodes(decision_tree, depth, x, y, height):
 	''' recursive function to plot the decision tree'''
 	# if the node is a leaf
-	if decision_tree['attribute'] == 0 and decision_tree['value'] == 0 \
-			and decision_tree['left'] == None and decision_tree['right'] == None:
+	if 'label' in decision_tree:
 		plt.text(
 			x, y, 
-			'X{dt[attribute]}<{dt[value]}'.format(dt=decision_tree),
+			'{dt[label]}'.format(dt=decision_tree),
 			size = height,
 			ha = 'center', 
 			va = 'center', 
 			bbox = dict(boxstyle='round', facecolor='white'), 
 			fontsize = 10 - np.minimum(depth, 5)
 		)
-	
+		return
 	plt.text(
 		x, y, 
 		'X{dt[attribute]}<{dt[value]}'.format(dt=decision_tree),
@@ -31,13 +28,13 @@ def nodes(decision_tree, depth, x, y, height):
 	if decision_tree['left'] != None :
 		x_left = x - 1/(2**(depth+2))
 		y_left = y - height
-		plt.plot([x, x_left], [y, y_left], marker='o')
+		plt.plot([x, x_left], [y, y_left])
 		nodes(decision_tree['left'], depth+1, x_left, y_left, height)
 
 	if decision_tree['right'] != None :
 		x_right = x + 1/(2**(depth+2))
 		y_right = y - height
-		plt.plot([x ,x_right] ,[y ,y_right], marker='o')
+		plt.plot([x ,x_right] ,[y ,y_right])
 		nodes(decision_tree['right'], depth+1, x_right, y_right, height)
 
 def visualize(dataset, depth=5):
@@ -49,8 +46,8 @@ def visualize(dataset, depth=5):
 	axes.set_xlim([0,1])
 	axes.set_ylim([0,1])
 	height = 1/depth
-
 	nodes(decision_tree, 0 , 0.5 , 1 , height )
-
 	plt.axis('off')
 	plt.show()		
+
+visualize(import_clean_data() )
