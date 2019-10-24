8b5e83ccdb098e3ec0d2029a6997bedc90cd78a9
paweenp <paween.pit@gmail.com>
2019-10-24 23:31:22 +0100

    create util.py and move import data functions to util

--- decision_tree.py
+++ decision_tree.py
@@ -1,11 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def import_clean_data():
-	return np.loadtxt('./dataset/clean_dataset.txt')
-
-def import_noisy_data():
-	return np.loadtxt('./dataset/noisy_dataset.txt')
+from util import *
 
 def H(labels):
 	''' return H-function of labels'''
