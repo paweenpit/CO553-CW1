d49ceb10e474b24529acdc6afcdd20142004d95e
paweenp <paween.pit@gmail.com>
2019-10-24 12:08:28 +0100

    refactor and comment evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -6,33 +6,41 @@ import numpy as np
 from decision_tree import decision_tree_learning, import_clean_data,\
         import_noisy_data
 
-test_size = 2000/10
+K = 10
+test_size = 2000/K
 
 
 # def create_fold(data):
-#     test_data_set, training test_data_set = np.split(data.copy(), [test_size], axis=0)
+#     test_dataset, training test_dataset = np.split(data.copy(), [test_size], axis=0)
 #     #first fold
-#     # test_data_set[:test_size], test_slice = test_slice, remainder[:test_size].copy()
+#     # test_dataset[:test_size], test_slice = test_slice, remainder[:test_size].copy()
 #     # #SECOND_FOLD
-#     # test_data_set[test_size:2*test_size], test_slice = test_slice, remainder[test_size:2*test_size].copy()
+#     # test_dataset[test_size:2*test_size], test_slice = test_slice, remainder[test_size:2*test_size].copy()
 #     # #thrid_fold
-#     print(test_data_set, trainig_data_set)
+#     print(test_dataset, trainig_data_set)
 
 
-def evaluate(test_data_set, trained_tree):
+def evaluate(test_dataset, trained_tree):
+    ''' return: confusion matrix, [avg recall, avg precision, F1 score, classification rate]'''
     confusion_matrix = np.zeros((4,4), dtype = int)
 
-    for sample in test_data_set:
+    # pass each sample in the trained_tree to get a predicted label
+    for sample in test_dataset:
         current_node = trained_tree
-        attribute_values = sample[0:-1]
+        attribute_values = sample[:-1]
         true_label = int(sample[-1])
+
         while True:
+            # if we find the leaf node
+            # update confusion matrix and exit
             if 'label' in current_node:
                 predicted_label = current_node['label']
                 confusion_matrix[true_label -1, predicted_label - 1] += 1
                 break
+            # iterate through left node if attribute value is less than the split node value
             elif attribute_values[current_node['attribute']] < current_node['value']:
                 current_node = current_node['left']
+            # iterate through right node if attribute value is not less than the split node value
             else:
                 current_node = current_node['right']
 
@@ -42,15 +50,22 @@ def evaluate(test_data_set, trained_tree):
 
 
 def metrics(confusion_matrix):
-    recall = np.zeros((1, np.shape(confusion_matrix)[0]), dtype = int)
-    precision = np.zeros((1, np.shape(confusion_matrix)[0]), dtype = int)
-    for label in range(np.shape(confusion_matrix)[0]):
-        recall[0:label] = confusion_matrix[label,label]/np.sum(confusion_matrix[label,:])
-        precision[0:label] = confusion_matrix[label,label]/np.sum(confusion_matrix[:,label])
+    ''' return: avg recall, avg precision, F1, classification rate'''
+    num_classes = confusion_matrix.shape[0]
+    recall = np.zeros(num_classes)
+    precision = np.zeros(num_classes)
+
+    for label in range(num_classes):
+        # recall = true positive / sum of all predicted positive
+        recall[label] = confusion_matrix[label,label]/np.sum(confusion_matrix[label,:])
+        # precision is true positive / sum of actual positive
+        precision[label] = confusion_matrix[label,label]/np.sum(confusion_matrix[:,label])
 
     average_recall = np.mean(recall)
     average_precision = np.mean(precision)
+
     F1 = 2*(average_precision*average_recall)/(average_precision + average_recall)
+    
     classification_rate = np.sum(np.diagonal(confusion_matrix))/np.sum(confusion_matrix)
     classification_error = 1 - classification_rate
 
@@ -61,8 +76,6 @@ def metrics(confusion_matrix):
 def test(data):
     tree, depth = decision_tree_learning(data, 0)
     print(evaluate(data, tree))
-
-
 
 
 if __name__ == '__main__':
