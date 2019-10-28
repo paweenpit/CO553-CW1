0c59eb815cd748bae33e3b6fdb7cf02792097594
Olle Nilsson <oen19@gpu05.doc.ic.ac.uk>
2019-10-28 15:47:04 +0000

    finished 10-fold validation

--- evaluation.py
+++ evaluation.py
@@ -10,10 +10,16 @@ K = 10
 test_size = 2000/K
 
 
-def create_fold(data, nr_of_folds = 10):
+def _10_fold_evaluation(data, nr_of_folds = 10):
 
     np.random.shuffle(data)
     folds = np.split(data, nr_of_folds)
+
+    recall_matrix = []
+    precision_matrix = []
+    F1_matrix = []
+    classification_rates = []
+    confusion_tensor = []
 
     for index in range(nr_of_folds):
         test_data_set = folds[index]
@@ -21,17 +27,41 @@ def create_fold(data, nr_of_folds = 10):
 
         tree, _ = decision_tree_learning(training_data_set, 0)
 
-        confusion_matrix , [average_recall, average_precision, F1, classification_rate]\
+        confusion_matrix , recall, precision, F1, classification_rate\
         = evaluate(test_data_set, tree)
 
-        print(confusion_matrix , [average_recall, average_precision, F1, classification_rate])
+        classification_rates.append(classification_rate)
+
+        if index == 0:
+            recall_matrix = recall
+            precision_matrix = precision
+            F1_matrix = F1
+            confusion_tensor = confusion_matrix
+        else:
+            recall_matrix = np.vstack((recall_matrix, recall))
+            precision_matrix = np.vstack((precision_matrix, precision))
+            F1_matrix = np.vstack((F1_matrix, F1))
+            confusion_tensor = np.dstack((confusion_tensor, confusion_matrix))
+
+        print(confusion_tensor[1])
+
+    average_recall = np.mean(recall_matrix, axis=0)
+    average_precision = np.mean(precision_matrix, axis=0)
+    average_F1 = np.mean(precision_matrix, axis=0)
+    average_classification_rate = np.mean(classification_rates)
 
 
 
+    # print(average_recall, average_precision, average_F1, average_classification_rate)
+
+    return(average_recall,
+            average_precision,
+            average_F1,
+            average_classification_rate)
 
 
 def evaluate(test_dataset, trained_tree):
-    ''' return: confusion matrix, [avg recall, avg precision, F1 score, classification rate]'''
+    ''' return: confusion matrix, recall, precision, F1 score, classification rate'''
     confusion_matrix = np.zeros((4,4), dtype = int)
 
     # pass each sample in the trained_tree to get a predicted label
@@ -54,9 +84,9 @@ def evaluate(test_dataset, trained_tree):
             else:
                 current_node = current_node['right']
 
-    average_recall, average_precision, F1, classification_rate = metrics(confusion_matrix)
+    recall, precision, F1, classification_rate = metrics(confusion_matrix)
 
-    return confusion_matrix , [average_recall, average_precision, F1, classification_rate]
+    return confusion_matrix, recall, precision, F1, classification_rate
 
 
 def metrics(confusion_matrix):
@@ -64,6 +94,7 @@ def metrics(confusion_matrix):
     num_classes = confusion_matrix.shape[0]
     recall = np.zeros(num_classes)
     precision = np.zeros(num_classes)
+    F1 = np.zeros(num_classes)
 
     for label in range(num_classes):
         # recall = true positive / sum of all predicted positive
@@ -71,15 +102,16 @@ def metrics(confusion_matrix):
         # precision is true positive / sum of actual positive
         precision[label] = confusion_matrix[label,label]/np.sum(confusion_matrix[:,label])
 
+        F1[label] = 2*(precision[label]*recall[label])/(precision[label] + recall[label])
+
     average_recall = np.mean(recall)
     average_precision = np.mean(precision)
-
-    F1 = 2*(average_precision*average_recall)/(average_precision + average_recall)
+    average_precision = np.mean(F1)
 
     classification_rate = np.sum(np.diagonal(confusion_matrix))/np.sum(confusion_matrix)
     classification_error = 1 - classification_rate
 
-    return average_recall, average_precision, F1, classification_rate
+    return recall, precision, F1, classification_rate
 
 
 def test(data):
@@ -89,5 +121,7 @@ def test(data):
 
 if __name__ == '__main__':
     clean_data = import_clean_data()
-    create_fold(clean_data)
+    noisy_data = import_noisy_data()
+    _10_fold_evaluation(clean_data)
+    _10_fold_evaluation(noisy_data)
 	# test(clean_data)
