3aa103dc5d5573ae1ca9d87dec60807fab70e498
paweenp <paween.pit@gmail.com>
2019-11-03 00:11:40 +0000

    remove deepcopy function

--- util.py
+++ util.py
@@ -11,34 +11,3 @@ def get_depth( tree ):
 	if 'label' in tree : 
 		return 0
 	return (max ( get_depth( tree['right'] ) , get_depth(tree['left']) ) + 1)
-
-''' credited to: https://stackoverflow.com/questions/47048366/implementing-a-copy-deepcopy-clone-function'''
-def deepcopy(data):
-    if isinstance(data, dict):
-        result = {}
-        for key, value in data.items():
-            result[key] = deepcopy(value)
-
-        assert id(result) != id(data)
-
-    elif isinstance(data, list):
-        result = []
-        for item in data:
-            result.append(deepcopy(item))
-
-        assert id(result) != id(data)
-
-    elif isinstance(data, tuple):
-        aux = []
-        for item in data:
-            aux.append(deepcopy(item))
-        result = tuple(aux)
-
-        assert id(result) != id(data)
-
-    elif isinstance(data, (int, float, type(None), str, bool)):
-        result = data
-    else:
-        raise ValueError("unexpected type")
-
-    return result
